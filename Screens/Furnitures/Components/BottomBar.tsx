be4ee53e8c6a742c2415206f6769f5529278e8fb
benjamin324132 <landavazobenjamin@gmail.com>
2023-02-13 15:47:04 -0700

    Add icons

--- Screens/Furnitures/Components/BottomBar.tsx
+++ Screens/Furnitures/Components/BottomBar.tsx
@@ -1,4 +1,10 @@
-//import {Filter, Heart, Home, SearchNormal1, ShoppingCart} from 'iconsax-react-native';
+import {
+  Filter,
+  Heart,
+  Home,
+  SearchNormal1,
+  ShoppingCart,
+} from 'iconsax-react-native';
 import React from 'react';
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {theme} from '../../../Utils/theme';
@@ -6,18 +12,18 @@ import {theme} from '../../../Utils/theme';
 export default function BottomBar(): JSX.Element {
   return (
     <View style={styles.container}>
-      {/*<View style={styles.icon}>
-      <Home size="32" color="#d9e3f0"/>
+      <View style={styles.icon}>
+        <Home size="32" color="#d9e3f0" />
       </View>
       <View style={styles.icon}>
-      <Heart size="32" color="#d9e3f0"/>
+        <Heart size="32" color="#d9e3f0" />
       </View>
       <View style={styles.icon}>
-      <SearchNormal1 size="32" color="#d9e3f0"/>
+        <SearchNormal1 size="32" color="#d9e3f0" />
       </View>
       <View style={styles.icon}>
-      <ShoppingCart size="32" color="#d9e3f0"/>
-  </View>*/}
+        <ShoppingCart size="32" color="#d9e3f0" />
+      </View>
     </View>
   );
 }
@@ -29,7 +35,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
   },
-  icon: {
-
-  },
+  icon: {},
 });
